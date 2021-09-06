56b16c0d77dce4e29f792b7fc54e78b2fab8e9f8
Stephen James <spjames95@gmail.com>
2021-09-06 16:23:39 +0100

    Add settings

---
+++ lua/settings.lua
@@ -0,0 +1,12 @@
+local utils = require 'utils'
+
+local current_dir = io.popen"cd":read'*l'
+
+local settingsPath = './.vim/settings.json';
+print(settingsPath)
+if not utils.fileExists(settingsPath) then
+    return {}
+end
+
+return vim.fn.json_decode(vim.fn.readfile(settingsPath)) or {}
+
