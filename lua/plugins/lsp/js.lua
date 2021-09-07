b9a8235218e135234ac032895ce85a6c44a52b8c
Stephen James <spjames95@gmail.com>
2021-09-07 11:38:03 +0100

    Add setting overrides for js

--- lua/plugins/lsp/js.lua
+++ lua/plugins/lsp/js.lua
@@ -1,2 +1,15 @@
 -- npm i -g tsserver typescript
-require'lspconfig'.tsserver.setup{}
+local utils = require 'utils'
+local projectSettings = require 'settings'
+
+local config = {}
+
+if globalSettings and globalSettings.lsp and globalSettings.lsp.tsserver then
+    config = utils.merge(config, globalSettings.lsp.tsserver)
+end
+
+if projectSettings.lsp and projectSettings.lsp.tsserver then
+    config = utils.merge(config, projectSettings.lsp.tsserver)
+end
+
+require'lspconfig'.tsserver.setup(config)
