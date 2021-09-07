e3b6d85dd8d939ad2c6dad7147dd8f8d7e4dfb1e
Stephen James <spjames95@gmail.com>
2021-09-07 09:05:17 +0100

    implement global settings

--- lua/plugins/lsp/php.lua
+++ lua/plugins/lsp/php.lua
@@ -1,32 +1,31 @@
 -- npm i -g intelephense
-local settings = require 'settings'
+local utils = require 'utils'
+local projectSettings = require 'settings'
 
-if settings.lsp and settings.lsp.intelephense then
-    settings = settings.lsp.intelephense
-else
-    settings = {}
-end
-
-local stubs = {
-    "apache", "bcmath", "bz2", "calendar", "com_dotnet", "Core", "ctype", "curl",
-    "date", "dba", "dom", "enchant", "exif", "FFI", "fileinfo", "filter", "fpm",
-    "ftp", "gd", "gettext", "gmp", "hash", "iconv", "imap", "intl", "json", "ldap",
-    "libxml", "mbstring", "meta", "mysqli", "oci8", "odbc", "openssl", "pcntl",
-    "pcre", "PDO", "pdo_ibm", "pdo_mysql", "pdo_pgsql", "pdo_sqlite", "pgsql",
-    "Phar", "posix", "pspell", "readline", "Reflection", "session", "shmop",
-    "SimpleXML", "snmp", "soap", "sockets", "sodium", "SPL", "sqlite3", "standard",
-    "superglobals", "sysvmsg", "sysvsem", "sysvshm", "tidy", "tokenizer", "xml",
-    "xmlreader", "xmlrpc", "xmlwriter", "xsl", "Zend OPcache", "zip", "zlib"
-}
-
-for k,v in pairs(settings.stubs or {}) do
-    stubs[table.getn(stubs)] = v
-end
-
-require 'lspconfig'.intelephense.setup({
+local config = {
     settings = {
         intelephense = {
-            stubs = stubs
+            stubs = {
+                "apache", "bcmath", "bz2", "calendar", "com_dotnet", "Core", "ctype", "curl",
+                "date", "dba", "dom", "enchant", "exif", "FFI", "fileinfo", "filter", "fpm",
+                "ftp", "gd", "gettext", "gmp", "hash", "iconv", "imap", "intl", "json", "ldap",
+                "libxml", "mbstring", "meta", "mysqli", "oci8", "odbc", "openssl", "pcntl",
+                "pcre", "PDO", "pdo_ibm", "pdo_mysql", "pdo_pgsql", "pdo_sqlite", "pgsql",
+                "Phar", "posix", "pspell", "readline", "Reflection", "session", "shmop",
+                "SimpleXML", "snmp", "soap", "sockets", "sodium", "SPL", "sqlite3", "standard",
+                "superglobals", "sysvmsg", "sysvsem", "sysvshm", "tidy", "tokenizer", "xml",
+                "xmlreader", "xmlrpc", "xmlwriter", "xsl", "Zend OPcache", "zip", "zlib"
+            }
         }
     }
-})
+}
+
+if globalSettings and globalSettings.lsp and globalSettings.lsp.intelephense then
+    config = utils.merge(config, globalSettings.lsp.intelephense)
+end
+
+if projectSettings.lsp and projectSettings.lsp.intelephense then
+    config = utils.merge(config, projectSettings.lsp.intelephense)
+end
+
+require 'lspconfig'.intelephense.setup(config)
--- lua/utils.lua
+++ lua/utils.lua
@@ -6,5 +6,41 @@ function utils.fileExists(name)
    if f~=nil then io.close(f) return true else return false end
 end
 
+function utils.merge(mergee, merger)
+    local merged = {}
+
+    for k,v in pairs(mergee) do
+        merged[k] = v
+    end
+
+    for mergerKey, mergerValue in pairs(merger) do
+        local mergeeValue = mergee[mergerKey]
+
+        if type(mergeeValue) == "table" and type(mergerValue) == "table" then
+            if utils.isArray(mergeeValue) and utils.isArray(mergerValue) then
+                for k,v in pairs(mergerValue) do
+                    local count = table.getn(mergeeValue)
+                    mergeeValue[count] = v
+                end
+            else
+                merged[mergerKey] = utils.merge(mergeeValue, mergerValue)
+            end
+        else
+            merged[mergerKey] = mergerValue
+        end
+    end
+
+    return merged
+end
+
+function utils.isArray(t)
+  local i = 0
+  for _ in pairs(t) do
+      i = i + 1
+      if t[i] == nil then return false end
+  end
+  return true
+end
+
 return utils
 
