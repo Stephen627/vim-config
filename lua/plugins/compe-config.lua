2ddf553fbdf20dd64e0c07b22f858be285014d76
Stephen James <me@stephenjames.dev>
2021-09-15 14:56:00 +0100

    remove tab functionality

--- lua/plugins/compe-config.lua
+++ lua/plugins/compe-config.lua
@@ -40,7 +40,3 @@ local check_back_space = function()
     end
 end
 
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
