6b20a4dedbac4cf4335039a08768fca3687c8c22
Stephen James <me@stephenjames.dev>
2022-12-02 16:03:09 +0000

    Fix line endings

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -67,9 +67,9 @@ lualine.setup({
 		icons_enabled = true,
 		theme = "auto",
 		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
 		always_divide_middle = true,
+        section_separators = { left = '', right = '' },
 	},
 	sections = {
 		lualine_a = { branch, diagnostics },
