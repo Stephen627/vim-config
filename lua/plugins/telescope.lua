819f02a0cbb5b5d3e4ffa66fb81850185d06e395
Stephen James <me@stephenjames.dev>
2022-12-02 16:03:38 +0000

    Remove unused commands

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,5 +7,3 @@ require('telescope').setup{
 
 vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap = true })
